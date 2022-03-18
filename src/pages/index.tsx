0aec5b3fa38da0b8c884d77d1ddf4042da4a1c22
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-03-18 17:39:58 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>inicio | ig.news</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
+  )
+}
