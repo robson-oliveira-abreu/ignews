13b5d5d5203c16a25b45fb2c7519b3a72b9a4ae4
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-04-04 21:51:54 -0300

    commit update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import Head from 'next/head'
 
@@ -6,6 +6,16 @@ import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
+
+// Client-side
+// Server-side
+// Static Site Generation
+
+// Post do blog
+// Conteudo (SSG)  //Sempre será o mesmo conteudo na pagina
+// Comentários (Client-side) //não é tão importante carregar primeiro
+
+// Cuidado com a performace do SSR pois cada api a mais chamada é mais demora para o client acessar o conteudo
 
 interface HomeProps {
   product: {
@@ -38,7 +48,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KkKM5BoEfgrlzpOrqDbjIzY')
 
   const product = {
@@ -52,6 +62,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
