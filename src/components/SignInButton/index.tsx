e8745fbfce34cbd5a188ccd6a9963b15bf7fe4cd
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-04-30 20:57:42 -0300

    commit update

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,28 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true
+    const { data: session } = useSession()
+    console.log(session)
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
             className={styles.signInButton}
             type="button"
+            onClick={() => signOut()}
         >
             <FaGithub color='#04d361' />
             Robson Oliveira
-            <FiX color='#737380' className={styles.closeIcon}/>
+            <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : (
         <button
             className={styles.signInButton}
             type="button"
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#eba417' />
             Sign in with GitHub
