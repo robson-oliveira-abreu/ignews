5254633135e2a7878a16bbf9315a0af070747ab1
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-08-15 23:48:03 -0300

    listening to stripe events

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -29,12 +29,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         let customerId = user.data.stripe_customer_id;
 
-        if(!customerId) {
+        if (!customerId) {
             const stripeCustomer = await stripe.customers.create({
                 email: session.user.email,
                 //metadata
             });
-    
+
             await fauna.query(
                 q.Update(
                     q.Ref(q.Collection('users'), user.ref.id),
